ba740cc716df6743da4705907cd811bdaa13cf97
han-yan-ds <hanprogramming@gmail.com>
2019-10-05 16:00:20 -0600

    Built out a simple express server

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const PORT = 4000;
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+// import controllerss
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// app.get('', getfunction);
+
+app.listen(PORT, () => `Server is listening on port ${PORT}`);
